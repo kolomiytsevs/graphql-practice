64dafe186234bdde303a190190ced308ba4ad101
Sasha Kolomiytsev <kolomiytsevs@googlemail.com>
2019-09-10 10:09:56 +0300

    Update gql resolvers

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -8,11 +8,65 @@ const productsTypeMatcher = {
   BIKE: 'Bike',
   DRONE: 'Drone'
 }
+//keys in the query object below have to be the same as the keys in the query object in product.gql
+//have to also create mutation resolvers. Create a new product or udate based on id.
+
+const products = () => {
+  return Product.find({})
+    .lean()
+    .exec()
+}
+
+const product = (_, args, ctx) => {
+  if (!ctx.user) throw new AuthenticationError()
+
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  if (!ctx.user) throw new AuthenticationError()
+
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const updateProduct = (_, args, ctx) => {
+  if (!ctx.user) throw new AuthenticationError()
+
+  const update = args.input
+
+  //passing in true means that the return vaue of the query is the product
+  //after it was updated not before (which is the default).
+  return Product.findByIdAndUpdate(args.id, update, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args, ctx) => {
+  if (!ctx.user) throw new AuthenticationError()
+
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
 
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
